3a96ee05410816c029e6d6ba1db0131e1bb650d7
nephinj <john.nephin@outlook.com>
2020-10-14 17:03:03 -0400

    fixed plan_id

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "azurerm_app_service" "Chatbot-svc" {
   name                = "${var.prefix}${var.KBLanguageCode}-svc"
   location            = var.location
   resource_group_name = var.resourceGroupName
-  app_service_plan_id = var.plan_id == "" ? azurerm_app_service_plan.Chatbot-svcplan.id : var.plan_id
+  app_service_plan_id = var.plan_id == "" ? azurerm_app_service_plan.Chatbot-svcplan[0].id : var.plan_id
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -263,5 +263,5 @@ output "key" {
 }
 
 output "plan_id" {
-  value = var.plan_id == "" ? azurerm_app_service_plan.Chatbot-svcplan.id : var.plan_id
+  value = var.plan_id == "" ? azurerm_app_service_plan.Chatbot-svcplan[0].id : var.plan_id
 }
