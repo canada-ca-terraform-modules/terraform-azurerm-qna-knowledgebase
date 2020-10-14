c032b94eafe1e3536a1321314836ee3c480a5a0b
nephinj <john.nephin@outlook.com>
2020-10-14 16:39:40 -0400

    fix variable bug

--- variables.tf
+++ variables.tf
@@ -47,6 +47,6 @@ variable "plan_reserved" {
 }
 
 variable "plan_kind" {
-  default = "Windows"
+  default = "Windows",
   description = "The kind of the App Service Plan to create. Possible values are Windows (also available as App), Linux, elastic (for Premium Consumption) and FunctionApp (for a Consumption Plan). Defaults to Windows. Changing this forces a new resource to be created."
 }
